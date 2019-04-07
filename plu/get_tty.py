9c09cc59d67de8671432f64e0170211c5a860923
Tillmann Heidsieck <theidsieck@leenox.de>
2019-04-07 12:36:31 +0200

    improve get tty <> sn mappings
    Signed-off-by: Tillmann Heidsieck <theidsieck@leenox.de>

--- plu/get_tty.py
+++ plu/get_tty.py
@@ -1,28 +1,16 @@
 #!/usr/bin/python3
+# SPDX-License-Identifier: MIT
+# TODO multiple ttys from one usb dev
 
 import os
 import re
 
+
 base_path = "/sys/bus/usb/devices"
 
-def get_tty(serial_number, sub_dev=0):
-    device = None
-    dev_path = ""
-    for d in os.listdir(base_path):
-        try:
-            dev_path = os.path.join(base_path, d)
-            sn_file = open(os.path.join(dev_path, "serial"), "r")
-            sn = sn_file.readline().strip()
-            sn_file.close()
-            if sn == serial_number:
-                device = d
-                break
-        except OSError:
-            continue
 
-    if not device:
-        return None
-
+def _get_tty(device, sub_dev=0):
+    dev_path = os.path.join(base_path, device)
     p = re.compile(r"tty.+[0-9]$")
     s = re.compile(device + ":1." + str(sub_dev))
     tty = None
@@ -42,8 +30,10 @@ def get_tty(serial_number, sub_dev=0):
 
     return tty
 
-def get_sn(tty=None):
+
+def get_tty_mappings():
     serial_numbers = []
+    ttys = []
     for d in os.listdir(base_path):
         try:
             dev_path = os.path.join(base_path, d)
@@ -51,15 +41,13 @@ def get_sn(tty=None):
             sn = sn_file.readline().strip()
             sn_file.close()
             serial_numbers.append(sn)
+            ttys.append(_get_tty(d))
         except OSError:
             continue
 
-    ttys = [get_tty(s) for s in serial_numbers]
-    
     return [{serial_numbers[i]: ttys[i]} \
             for i in range(0, len(ttys)) if ttys[i] != None]
 
 
 if __name__ == "__main__":
-    print(get_tty("A400D61R"))
-    print(get_sn())
+    print(get_tty_mappings())
